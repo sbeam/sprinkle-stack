ec818ba942b8a07d75dca7a07cb950b1a4edf07b
Sam Beam <sbeam@onsetcorps.net>
2012-09-06 12:27:14 -0400

    sudo does not need apache

--- config/stack/sudo.rb
+++ config/stack/sudo.rb
@@ -3,10 +3,9 @@ require File.join(File.dirname(__FILE__), '../config.rb')
 package :sudo do
   description 'Installs sudo command'
   apt %w( sudo )
-  
+
   verify do
     has_executable 'sudo'
   end
-  
-  requires :apache
+
 end
